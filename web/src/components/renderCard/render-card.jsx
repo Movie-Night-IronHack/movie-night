9e5d428360ccddce0854656199b64d2d3257d80a
ahaddadl <ahaddad@vocento.com>
2024-12-13 17:56:18 +0100

    Pintado de los posters

---
+++ web/src/components/renderCard/render-card.jsx
@@ -0,0 +1,42 @@
+function RenderCard({ movie }) {
+  console.log(movie);
+  return (
+    <div className="card event-item" style={{width: "18rem"}}>
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="card-img-top" alt={movie.original_title} />
+      <div className="card-body">
+        <h5 className="card-title mb-1 fw-light text-break">
+          {movie.original_title}
+        </h5>
+      </div>
+    </div>
+  );
+}
+export default RenderCard;
+
+{
+  /* <div className="card event-item">
+<img src={event.poster} className="card-img-top" alt={event.title} />
+<div className="card-body">
+  <h5 className="card-title mb-1 fw-light text-break">
+    <Link to={`/events/${event.id}`}>{event.title}</Link>
+  </h5>
+  <p className="mb-0 fs-xs">
+    <strong>{dayjs(event.eventDate).format("lll")}</strong>
+  </p>
+  <p className="text-muted fw-lighter fs-xs">{event.location}</p>
+  <div className="d-flex gap-1 flex-wrap mb-1">
+    {event.categories.map((category) => (
+      <span key={category} className="badge text-bg-light">
+        {category}
+      </span>
+    ))}
+  </div>
+  <button
+    className="btn btn-sm btn-danger"
+    onClick={() => onDelete(event)}
+  >
+    delete
+  </button>
+</div>
+</div> */
+}
--- web/src/components/renderCards/render-cards.jsx
+++ web/src/components/renderCards/render-cards.jsx
@@ -1,15 +1,27 @@
-import { useEffect } from 'react';
-import * as MovieApi from '../../services/api-service'
+import { useEffect, useState } from "react";
+import * as MovieApi from "../../services/api-service";
+import RenderCard from "../renderCard/render-card";
 
-function RenderCards() {
- useEffect(()=>{
+function RenderCards({ className = "" }) {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+
     MovieApi.trendingMovies()
-        .then((movies)=> console.log(movies))
- },[])
+      .then((movies) => setMovieList(movies.results))
+      .catch((error)=> console.error(error));
+    
+
+  }, []);
 
   return (
     <>
       <h2>Render Cards</h2>
+      <div className={`d-flex flex-wrap gap-3 ${className}`}>
+        {movieList.map((movie)=>(
+          <RenderCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </>
   );
 }
