445725f40949570212f6ea02d6c1e55f92f80eff
RoniPG <ronigmn@gmail.com>
2025-01-11 18:01:40 +0100

    Styling and Fixing SideBar

--- web/src/components/sidebar/side-bar.jsx
+++ web/src/components/sidebar/side-bar.jsx
@@ -1,29 +1,41 @@
+import { useState } from "react";
 import "./side-bar.css";
 
 function SideBar() {
+  const [selectedButton, setSelectedButton] = useState(null);
+  const handleClick = (btnName) => {
+    setSelectedButton(btnName);
+  }
   return (
     <div
       className=" fixed-top d-flex flex-rows mt-5 gap-5 justify-content-center text-white"
       style={{ minWidth: "100px" }}
     >
-      <button 
-        className="btn btn-sm mt-3 bg-light rounded">
-        <a href="#list-cards" style={{color:"rgb(105,105,105)"}} className="fs-5 sidebar-a">
+      <button
+        onClick={() => handleClick("list-cards")}
+        className={`btn btn-sm mt-3 rounded ${selectedButton === "list-cards" ? "btn-selected" : "bg-light"}`}>
+        <a href="#list-cards" style={{textDecoration:"none", color:"grey"}} className="fs-5">
           Trending
         </a>
       </button>
-      <button className="btn btn-sm mt-3 bg-light rounded">
-        <a href="#now-playing" style={{color:"rgb(105,105,105)"}} className="fs-5 sidebar-a">
+      <button
+        onClick={() => handleClick("now-playing")}
+        className={`btn btn-sm mt-3 rounded ${selectedButton === "now-playing" ? "btn-selected" : "bg-light"}`}>
+        <a href="#now-playing" style={{textDecoration:"none", color:"grey"}} className="fs-5">
           Now Playing
         </a>
       </button>
-      <button className="btn mt-3 btn-sm bg-light rounded">
-        <a href="#top-rated" style={{color:"rgb(105,105,105)"}} className="fs-5 sidebar-a ">
+      <button
+        onClick={() => handleClick("top-rated")}
+        className={`btn btn-sm mt-3 rounded ${selectedButton === "top-rated" ? "btn-selected" : "bg-light"}`}>
+        <a href="#top-rated" style={{textDecoration:"none", color:"grey"}} className="fs-5">
           Top Rated
         </a>
       </button>
-      <button className="btn btn-sm mt-3 bg-light rounded">
-        <a href="#upcoming" style={{color:"rgb(105,105,105)"}} className="fs-5 sidebar-a ">
+      <button
+        onClick={() => handleClick("upcoming")}
+        className={`btn btn-sm mt-3 rounded ${selectedButton === "upcoming" ? "btn-selected" : "bg-light"}`}>
+        <a href="#upcoming" style={{textDecoration:"none", color:"grey"}} className="fs-5">
           Upcoming
         </a>
       </button>
